21de32ef091ddb6b5060c1a25d1bcba7cdb88100
givikooo <givi.chlikadze@gmail.com>
2022-06-05 23:53:46 +0400

    Update contact responsive

--- src/Pages/Contact.tsx
+++ src/Pages/Contact.tsx
@@ -9,6 +9,7 @@ import BurgerMenu from '../Components/BurgerMenu';
 import Burger from '../Components/Burger';
 import { useNavigate } from 'react-router-dom';
 import Colors from './../assets/colors';
+import Responsive from '../Responsive';
 
 const Contact = () => {
     const [name, setName] = useState('')
@@ -16,9 +17,9 @@ const Contact = () => {
     const [mobile, setMobile] = useState('')
     const [mail, setMail] = useState('')
     const [question, setQuestion] = useState('')
-    
 
-   
+
+
 
     return (
         <>
@@ -105,13 +106,36 @@ const Body = styled.section`
     height: 100vh;
     display: flex;
     justify-content: space-around;
+    ${Responsive.tablet}{
+        flex-direction: column-reverse;
+        align-items: center;
+        justify-content: flex-start;
+    }
+    ${Responsive.mobile}{
+        flex-direction: column-reverse;
+        align-items: center;
+        justify-content: flex-start;
+    }
 `
 const FormWrapper = styled.form`
     width: 650px;
     margin-top: 300px;
+    ${Responsive.tablet}{
+        margin-top: unset;
+    }
+    ${Responsive.mobile}{
+        margin-top: unset;
+        width: 100%;
+    }
 `
 const RightSide = styled.div`
     margin-top: 430px;
+    ${Responsive.tablet}{
+        margin-top: unset;
+    }
+    ${Responsive.mobile}{
+        margin-top: unset;
+    }
 `
 
 const Title = styled.h1`
@@ -121,6 +145,12 @@ const Title = styled.h1`
     font-family: 'Roboto';
     font-size: 48px;
     line-height: 56px;
+    ${Responsive.tablet}{
+        font-size: 32px;
+    }
+    ${Responsive.mobile}{
+        font-size: 24px;
+    }
 `
 const ContactText = styled.p`
     color: ${Colors.Black};
